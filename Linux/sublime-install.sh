1d45137e5e3dd00f232702142506428a793b5791
Siegfried5 <rudolph.ponce745@gmail.com>
2016-02-27 19:32:03 -0700

    added sublime installing

---
+++ Linux/sublime-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Rudolph Ponce
+#Sat Feb 27 19:29:29 MST 2016
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
