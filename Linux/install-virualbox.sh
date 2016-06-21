1f153ba7b8be124a675a580b380995c22b5f570b
root <root@localhost.localdomain>
2016-06-21 09:35:55 -0700

    added virtualbox-setup.sh

--- Linux/install-virualbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Author: Rudolph Ponce
-# Date: 22, October 2015
-# Note: run this script as root
-
-sudo apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian vivid contrib"
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install virtualbox-5.0
---
+++ Linux/virtualbox-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Rudolph Ponce
+# Date: 22, October 2015
+# Note: run this script as root
+
+apt-get install virtualbox-guest-additions-iso virtualbox-guest-source virtualbox-guest-utils virtualbox-guest-x11 virtualbox-guest-dkms 
