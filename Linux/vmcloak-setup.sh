0f182e8964de1915063fb5ea576eeb0a23bbc2b5
Siegfried5 <rudolph.ponce745@gmail.com>
2016-12-17 12:05:33 -0700

    added vmcloak-setup.sh

---
+++ Linux/vmcloak-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $HOME
+
+if ! git="$(type -p "$git")" || [ -z "$git" ]
+then
+  sudo apt-get install git
+done
+cd $HOME
+git clone https://github.com/jbremer/vmcloak
+
+cd vmcloak
+sudo python setup.py install
