980634021527992a8801d9ed8b44492dca13dd7d
Siegfried5 <rudolph.ponce745@gmail.com>
2016-12-17 12:23:12 -0700

    added pip and setuptools to the install

--- Linux/vmcloak-setup.sh
+++ Linux/vmcloak-setup.sh
@@ -4,7 +4,7 @@ cd $HOME
 
 if ! git="$(type -p "$git")" || [ -z "$git" ]
 then
-  sudo apt-get install git
+  sudo apt-get install git python-pip python-setuptools
 fi
 cd $HOME
 git clone https://github.com/jbremer/vmcloak
